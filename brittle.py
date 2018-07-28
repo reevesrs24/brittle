adbbdf98569f1074514a03237af3582a2bc46901
reevesrs24 <reevesrs24@gmail.com>
2018-07-27 22:18:21 -0500

    added port mapping

--- brittle.py
+++ brittle.py
@@ -4,9 +4,34 @@ import getopt
 import requests
 import re
 
+
 ## Global Variables ##
 store = False
 discover = False
+
+
+def add_port_mapping():
+
+    payload =   """<?xml version="1.0" encoding="utf-8"?>
+                <s:Envelope>
+                    <s:Body>
+                        <u:AddPortMapping xmlns:u="urn:schemas-upnp-org:services:WANIPConnections:1"> 
+                            <NewRemoteHost></NewRemoteHost> 
+                            <NewExternalPort>1234</NewExternalPort> 
+                            <NewInternalClient>192.168.1.50</NewInternalClient> 
+                            <NewInternalPort>1234</NewInternalPort> 
+                            <NewProtocol>TCP</NewProtocol> 
+                            <NewPortMappingDescription>test</NewPortMappingDescription> 
+                            <NewLeaseDuration>1000</NewLeaseDuration> 
+                            <NewEnabled>1</NewEnabled> 
+                        </u:AddPortMapping> 
+                    </s:Body> 
+                </s:Envelope>"""
+
+    headers = { 'content-type': 'text/xml', 'SOAPAction' : 'urn:schemas-upnp-org:service:WANIPConnection:1#AddPortMapping' }
+    r = requests.post('http://192.168.1.1:34753/ctl/IPConn', headers=headers, data=payload )
+    print r.content
+    
 
 
 def store_location(data):
@@ -14,9 +39,6 @@ def store_location(data):
     location = re.search("(?i)Location: (.+?)\r\n", data)
     if location:
         print location.group(1)
-
-
-    
 
 
 
@@ -54,9 +76,11 @@ def main():
     global store
     global discover
 
+
+
     # read the command line options
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "ds", ["discover", "store"])
+        opts, args = getopt.getopt(sys.argv[1:], "dsqm", ["discover", "store", "quit", "mapping"])
     except getopt.GetoptError as err:
         print str(err)
         sys.exit(-1)
@@ -66,6 +90,10 @@ def main():
             discover = True
         elif o in ("-s", "--store"):
             store = True
+        elif o in ("-q", "--quit"):
+            sys.exit(0)
+        elif o in ("-m", "--mapping"):
+            add_port_mapping()
         else:
             assert False, "Unhandled Option"
 
